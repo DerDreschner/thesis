76a6ab4fa3329e280437b1cf0042989c1bce82da
David Dreschner <d.dreschner@kuehlhaus.com>
2019-05-23 17:06:38 +0200

    [TASK] PRODDR-13: First implementation of frontend (without real logic)

---
+++ src/Feature/UserInterface/code/Pipelines/AddScriptsToContentEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.UI;
+using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.StringExtensions;
+
+namespace SitecoreUrlShorter.Feature.UserInterface.Pipelines
+{
+    // ReSharper disable once UnusedMember.Global
+    public class AddScriptsToContentEditor
+    {
+        private const string JavascriptTag = "<script src=\"{0}\"></script>";
+        private const string StylesheetLinkTag = "<link href=\"{0}\" rel=\"stylesheet\" />";
+
+        public void Process(PipelineArgs args)
+        {
+            AddControls(JavascriptTag, "SitecoreUrlShorter.Feature.UserInterface.JavaScript");
+            AddControls(StylesheetLinkTag, "SitecoreUrlShorter.Feature.UserInterface.Stylesheet");
+        }
+
+        private static void AddControls(string resourceTag, string configKey)
+        {
+            Assert.IsNotNullOrEmpty(configKey, "Content Editor resource config key cannot be null");
+
+            var resource = Settings.GetSetting(configKey);
+
+            if (string.IsNullOrEmpty(resource)) return;
+
+            Context.Page.Page.Header.Controls.Add(new LiteralControl(resourceTag.FormatWith(resource)));
+        }
+    }
+}
