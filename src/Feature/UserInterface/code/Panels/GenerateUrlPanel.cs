984874b8fe5cbc01e748e0b88073894f2a2336aa
David Dreschner <d.dreschner@kuehlhaus.com>
2019-05-23 17:40:51 +0200

    [CLEANUP] PRODDR-13: Fix inspection warnings

--- src/Feature/UserInterface/code/Panels/GenerateUrlPanel.cs
+++ src/Feature/UserInterface/code/Panels/GenerateUrlPanel.cs
@@ -1,9 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using Sitecore.Configuration;
 using Sitecore.Data.Items;
-using Sitecore.Diagnostics;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Shell.Web.UI.WebControls;
 using Sitecore.Web.UI.WebControls.Ribbons;
@@ -13,13 +10,13 @@ namespace SitecoreUrlShorter.Feature.UserInterface.Panels
     // ReSharper disable once UnusedMember.Global
     public class GenerateUrlPanel : RibbonPanel
     {
-
         [SuppressMessage("ReSharper", "StringLiteralTypo")]
         public override void Render(HtmlTextWriter output, Ribbon ribbon, Item button, CommandContext context)
         {
             var htmlOutput = string.Empty;
 
-            htmlOutput += "<div style='line-height:40px'><button style='vertical-align: middle;' onclick='javascript:return scForm.invoke('contenteditor:home', event)'>Generate new URL</button></div>";
+            htmlOutput +=
+                "<div style='line-height:40px'><button style='vertical-align: middle;' onclick='javascript:return scForm.invoke('contenteditor:home', event)'>Generate new URL</button></div>";
 
             output.Write(htmlOutput);
         }
--- src/Feature/UserInterface/code/Panels/ShowUrlPanel.cs
+++ src/Feature/UserInterface/code/Panels/ShowUrlPanel.cs
@@ -1,9 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using Sitecore.Configuration;
 using Sitecore.Data.Items;
-using Sitecore.Diagnostics;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Shell.Web.UI.WebControls;
 using Sitecore.Web.UI.WebControls.Ribbons;
@@ -13,13 +10,13 @@ namespace SitecoreUrlShorter.Feature.UserInterface.Panels
     // ReSharper disable once UnusedMember.Global
     public class ShowUrlPanel : RibbonPanel
     {
-
         [SuppressMessage("ReSharper", "StringLiteralTypo")]
         public override void Render(HtmlTextWriter output, Ribbon ribbon, Item button, CommandContext context)
         {
             var htmlOutput = string.Empty;
 
-            htmlOutput += "<div style='line-height:40px;display:flex;'><input type='text' value='http://shrtsvc.local/23io3dso' size='27' id='shortUrl' style='vertical-align: middle;' disabled><input type='image' src='/temp/iconcache/office/32x32/clipboard_paste.png' onclick=\"return copyToClipboard('shortUrl')\"></div>";
+            htmlOutput +=
+                "<div style='line-height:40px;display:flex;'><input type='text' value='http://shrtsvc.local/23io3dso' size='27' id='shortUrl' style='vertical-align: middle;' disabled><input type='image' src='/temp/iconcache/office/32x32/clipboard_paste.png' onclick=\"return copyToClipboard('shortUrl')\"></div>";
 
             output.Write(htmlOutput);
         }
