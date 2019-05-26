a0fd69d0d38df222b470cefc47e643c050a95fef
David Dreschner <d.dreschner@kuehlhaus.com>
2019-05-26 07:24:16 +0200

    [TASK] PRODDR-7: Include documentation for administrators and editors

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding AfterBuild='03:local:sitecore:init' />
+/// <binding AfterBuild='03:local:sitecore:init' ProjectOpened='04:leprechaun:generate:watch' />
 require("./scripts/gulp/local.js");
 require("./scripts/unicorn/gulpfile.js");
 require("./build/gulpfile.js");
