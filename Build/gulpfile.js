6db6e460ff5e0c5242023ee4e529665437b0669e
David Dreschner <d.dreschner@kuehlhaus.com>
2019-05-26 07:34:22 +0200

    [CLEANUP] PRODDR-15: Remove junk from build gulpfile

--- Build/gulpfile.js
+++ Build/gulpfile.js
@@ -14,84 +14,23 @@ var webRoot = gulpConfig.webRoot
 
 module.exports.config = gulpConfig
 
-function cleanProjectFiles(layerName) {
-  const filesToDelete = [
-    webRoot + "/bin/AktionMensch." + layerName + ".*",
-    webRoot + "/App_Config/Include/" + layerName + "*",
-    webRoot + "/App_Config/Include/Z." + layerName + "*"
-  ]
-
-  console.log("Removing " + layerName + " configs/binaries")
-
-  return gulp.src(filesToDelete, { read: false })
-    .pipe(vinylPaths(paths => del(paths, { force: true })));
-}
-cleanProjectFiles.displayName = "00:publish:layer:cleanup"
-
 function cleanAllProjectFiles() {
   const filesToDelete = [
-    webRoot + "/App_Config/**/*.ji2",
-    webRoot + "/App_Config/**/*.exclude",
-    webRoot + "/App_Config/Include/Foundation/*",
-    webRoot + "/App_Config/Include/Feature/*",
-    webRoot + "/App_Config/Include/Z.Feature/*",
-    webRoot + "/App_Config/Include/Project/*",
-    webRoot + "/App_Config/Include/Z.Project/*",
-    webRoot + "/App_Config/Include/Z/*",
-    webRoot + "/App_Config/Include/Z.Support/*",
-    webRoot + "/App_Config/Include/Z_CMS/*",
+    webRoot + "/bin/SitecoreUrlShorter.*",
+    webRoot + "/App_Config/Include/SitecoreUrlShorter/*",
     webRoot + "/App_Config/Include/Unicorn/*",
-    webRoot + "/Areas/*",
-    webRoot + "/bin/AktionMensch.*",
-    webRoot + "/bin/Castle.Windsor.*",
+	webRoot + "/App_Config/Include/Rainbow.config",
+	webRoot + "/App_Config/Environment/Foundation/Foundation.Data.config",
     webRoot + "/bin/Rainbow.*",
-    webRoot + "/bin/Sitecore.Support.*",
-    webRoot + "/bin/Unicorn.*",
-    webRoot + "/Views/*",
-    "!" + webRoot + "/App_Config/Include/Foundation/Foundation.Search.ServiceAddress.config",
-    "!" + webRoot + "/Views/*.config",
-    "!" + webRoot + "/Views/Shared"
+    webRoot + "/bin/Unicorn.*"
   ]
 
-  console.log("Removing old configs, binaries and views")
+  console.log("Removing old configs and binaries")
 
   return gulp.src(filesToDelete, { read: false, allowEmpty: true })
     .pipe(vinylPaths(paths => del(paths, { force: true })));
 }
-cleanAllProjectFiles.displayName = "00:publish:solution:cleanup:configs"
-
-function publishProjects(location, dest) {
-  dest = dest || webRoot
-  var targets = ["Build"]
-
-  console.log("publish to " + dest + " folder")
-  return gulp.src([location + "/**/code/*.csproj"])
-    .pipe(foreach(function (stream) {
-      return stream
-        .pipe(debug({ title: "Building project:" }))
-        // @ts-ignore
-        .pipe(msbuild({
-          targets: targets,
-          configuration: gulpConfig.buildConfiguration,
-          logCommand: false,
-          verbosity: "minimal",
-          stdout: true,
-          errorOnFail: true,
-          maxcpucount: 0,
-          // @ts-ignore
-          toolsVersion: gulpConfig.MSBuildToolsVersion,
-          properties: {
-            DeployOnBuild: "true",
-            DeployDefaultTarget: "WebPublish",
-            WebPublishMethod: "FileSystem",
-            DeleteExistingFiles: "false",
-            TransformWebConfigEnabled: "false",
-            AutoParameterizationWebConfigConnectionStrings: "false",
-            publishUrl: dest
-          }
-        }))
-    }))
-}
+cleanAllProjectFiles.displayName = "00:publish:solution:cleanup"
 
 function cleanupSolution() {
   var targets = ["Clean"]
@@ -170,28 +109,6 @@ gulp.task("00:publish:solution", function () {
       }
     }))
 })
-
-gulp.task("00:publish:layer:foundation", function () {
-  return cleanProjectFiles("Foundation"),
-    publishProjects(devRoot + "/src/Foundation")
-})
-
-gulp.task("00:publish:layer:feature", function () {
-  return cleanProjectFiles("Feature"),
-    publishProjects(devRoot + "/src/Feature")
-})
-
-gulp.task("00:publish:layer:project", function () {
-  return cleanProjectFiles("Project"),
-    publishProjects(devRoot + "/src/Project")
-})
-
-gulp.task("00:publish:helix", gulp.series(
-  gulp.parallel("00:publish:layer:foundation",
-    "00:publish:layer:feature",
-    "00:publish:layer:project"),
-    "03:local:env:start"
-));
 
 gulp.task("00:publish:solution", gulp.series(
   cleanAllProjectFiles,
